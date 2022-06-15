9e7f03f032664cc6a62ca333db685c618eb55007
Joshua <link7844@gmail.com>
2022-06-15 16:29:03 -0700

    delete and validate todos

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import picture from "../assets/Picture.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+          <div>
+            <div class="split left">
+              <div className="centered">
+                <img 
+                  className="profile_image"
+                  src={picture}
+                  alt="Profile Pic"
+                ></img>
+              </div>
+            </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Joshua Holdridge</div>
+              <div className="brief_description">
+                My name is Joshua Holdridge, I usually go by Josh. I am a Computer Science student at the University of Nevada, Las Vegas. I love to play fighting games and learn about programming.
+              </div>
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
